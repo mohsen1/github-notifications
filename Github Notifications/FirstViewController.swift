cebf6db58d540f79647279e2859207657e4a28d0
Mohsen Azimi <msn.azi@gmail.com>
2015-01-18 21:27:37 -0800

    Initial Commit

---
+++ Github Notifications/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Github Notifications
+//
+//  Created by Mohsen Azimi on 1/18/15.
+//  Copyright (c) 2015 Mohsen Azimi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
